0822652be915b726e080297f8c3f51c4f3bd078a
melheins <heins.mel@gmail.com>
2017-09-16 14:02:09 -0400

    push to html

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
     var endYr;
 
 
-    $(".btn").click(function () {
+    $(".btn[type='submit']").click(function () {
 
         // Stop screen from refreshing
         event.preventDefault();
@@ -20,13 +20,13 @@ $(document).ready(function () {
         console.log("On click");
 
         //Set search values
-        searchTerm =  document.getElementById("search-term").value;
+        searchTerm = document.getElementById("search-term").value;
         //numRecords =  document.getElementById("num-records").value;
-        startYr =  document.getElementById("begin-year").value;
-        endYr =  document.getElementById("end-year").value;
+        startYr = document.getElementById("begin-year").value;
+        endYr = document.getElementById("end-year").value;
 
         console.log(searchTerm);
-       // console.log(numRecords);
+        // console.log(numRecords);
         console.log(startYr);
         console.log(endYr);
 
@@ -70,9 +70,35 @@ $(document).ready(function () {
 
             console.log(retrivedData.response.docs[0].web_url);
 
+
+            for (var i = 0; i < numRecords; i++) {
+                var newDiv = $("<div>");
+                var headline = $("<h1>");
+                var byLine = $("<h5>");
+                var datePub = $("<h4>");
+                var articleUrl = $("<a>");
+
+                headline.text(retrivedData.response.docs[i].headline.main);
+                byLine.text(retrivedData.response.docs[i].byline.original);
+                datePub.text(retrivedData.response.docs[i].pub_date);
+                articleUrl.attr('href', retrivedData.response.docs[i].web_url);
+
+                newDiv.append(headline);
+                newDiv.append(byLine);
+                newDiv.append(datePub);
+                newDiv.append(articleUrl);
+
+                $('#results').append(newDiv);
+            }
         })
 
-    })
+    });
+
+
+    $(".btn[type='del']").click(function () {
+
+
+    });
 
 
 });
